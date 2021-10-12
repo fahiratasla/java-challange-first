ccc7f74a670b9473f534e977222eac255f0f934b
Fahira <fahiratasla13@gmail.com>
2021-10-12 16:15:17 +0700

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        String firstName, lastName, alamat, makanankesukaan, hobi, phoneNumber;
+        int age;
+        double tinggi;
+        
+        firstName= "Fahira ";
+        lastName= "Tasla";
+        age= 21;
+        tinggi= 163.3;
+        alamat= "Medan";
+        makanankesukaan= "Ayam Geprek";
+        hobi= "badminton";
+        phoneNumber= "081234759";
+        
+        System.out.println("firstName= " + "Fahira");
+        System.out.println("lastName= " + "Tasla");
+        System.out.println("age= " + "21");
+        System.out.println("tinggi= " + "163.3");
+        System.out.println("alamat= " + "Medan");
+        System.out.println("makanankesukaan= " + "Ayam Geprek");
+        System.out.println("hobi= " + "badminton");
+        System.out.println("phoneNumber= " + "081234759");
+        
+    }
+
+
+}
